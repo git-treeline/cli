5ee895aa3be05e349db53f3c62f2c261cf9ff858
Jonathan Simmons <jonathan@productmatter.co>
2026-06-22 14:38:49 -0500

    feat: add gtl version --json with cli and router versions

--- cmd/version.go
+++ cmd/version.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/git-treeline/cli/internal/service"
 	"github.com/spf13/cobra"
 )
 
@@ -11,12 +13,28 @@ var Version = "dev"
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	versionCmd.Flags().Bool("json", false, "Output version info as JSON")
 }
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version",
 	Run: func(cmd *cobra.Command, args []string) {
+		asJSON, _ := cmd.Flags().GetBool("json")
+		if asJSON {
+			router := service.RunningRouterVersion()
+			out := map[string]interface{}{
+				"cli":    Version,
+				"router": nil,
+			}
+			if router != "" {
+				out["router"] = router
+			}
+			enc := json.NewEncoder(cmd.OutOrStdout())
+			enc.SetIndent("", "  ")
+			_ = enc.Encode(out)
+			return
+		}
 		fmt.Printf("git-treeline %s\n", Version)
 	},
 }
